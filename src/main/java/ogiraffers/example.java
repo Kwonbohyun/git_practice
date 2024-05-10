21cb27a56a1e3d6ee3c613b7ea52deb1e4f07225
Kwonbohyun <kwon18923@gmail.com>
2024-05-10 10:56:33 +0900

    first commit

---
+++ src/main/java/ogiraffers/example.java
@@ -0,0 +1,10 @@
+package ogiraffers;
+
+public class example {
+       public static void main(String[] args) {
+
+           System.out.print("Hello World!");
+           System.out.print("한글은 깨져요");
+       }
+}
+
